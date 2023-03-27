034fa660d2811906884f011c62450c7490066651
Andrew-Sparrow <8911102@gmail.com>
2023-03-27 23:25:27 +0500

    adds type and  port to ship card

--- src/components/ship/ship.js
+++ src/components/ship/ship.js
@@ -5,6 +5,8 @@ function Ship() {
   return (
     <div className='ship'>
       <h2 className='ship__title'> Ship title</h2>
+      <p className='ship__info'><span className='ship__detail'>Тип</span>asdfa </p>
+      <p className='ship__info'><span className='ship__detail'>Порт</span>fffff</p>
     </div>
   )
 }
