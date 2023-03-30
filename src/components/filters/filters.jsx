8ef00ce5532e94642c78c13bd62674ceb1d4ad1d
Andrew-Sparrow <8911102@gmail.com>
2023-03-30 23:01:48 +0500

    adds some styles for react-select menu

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -3,6 +3,7 @@ import { HandySvg } from 'handy-svg';
 
 import styles from './filters.module.scss';
 import iconArrowLeftSVG from '../../img/icons/Arrow_Left.svg';
+import { FilterSelect } from '../filter-select/filter-select';
 
 const IconArrowLeft = ({ onClick }) => (
   <HandySvg
@@ -26,6 +27,8 @@ const Filters = ({ onClick }) => {
         <div className={styles.input_wrapper}>
           <input className={styles.input} type="text" id="name" />
         </div>
+        <label className={styles.label} htmlFor="name">Порт</label>
+        <FilterSelect />
       </section>
     </aside>
   )
