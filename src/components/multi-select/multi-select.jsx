ebe4268900d7846628b63dda884e7bb2768db923
Andrew-Sparrow <8911102@gmail.com>
2023-03-31 20:14:14 +0500

    adds logic for calculate clicked checkboxes

--- src/components/multi-select/multi-select.jsx
+++ src/components/multi-select/multi-select.jsx
@@ -1,25 +1,59 @@
 import React, { useState } from 'react';
+import { HandySvg } from 'handy-svg';
+
 
 import  multiSelectStyles from './multi-select.module.scss';
 import  filterStyles from '../filters/filters.module.scss';
 import { OutsideAlerter } from '../outside-alerter/outside-alerter';
+import iconCheckBoxYesSVG from '../../img/icons/CheckBox_Yes.svg';
+import iconCheckBoxNoSVG from '../../img/icons/CheckBox_No.svg';
+
 
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' }
-]
+];
+
+const IconCheckBoxYes = () => (
+  <HandySvg
+    src={iconCheckBoxYesSVG}
+    width="24"
+    height="24"
+  />
+);
+
+const IconCheckBoxNo = () => (
+  <HandySvg
+    src={iconCheckBoxNoSVG}
+    width="24"
+    height="24"
+  />
+);
 
 const MultiSelect = () => {
-  const [isSelectActive, setIsSelectActive] = useState(false);
+  const [isSelectOpen, setIsSelectOpen] = useState(false);
+  const [selectedValues, setSelectedValues] = useState([]);
 
   const onClickSelectHandler = (evt) => {
-    setIsSelectActive((prev) => !prev);
+    setIsSelectOpen((prev) => !prev);
   };
 
   const onClickOutsideHandler = () => {
-    setIsSelectActive(false);
+    setIsSelectOpen(false);
   }
+
+  const onSelectedItemClick = (value) => {
+    if (selectedValues.includes(value)) {
+      setSelectedValues((prev) => prev.filter((item) => item !== value));
+    } else {
+      setSelectedValues((prev) => [...prev, value]);
+    }
+  };
+
+  const isSelected = (value) => {
+    return selectedValues.includes(value);
+  };
 
   return (
     <>
@@ -27,12 +61,22 @@ const MultiSelect = () => {
       <OutsideAlerter onClickSelectHandle={onClickOutsideHandler}>
         <div className={multiSelectStyles.select_wrapper} >
           <div className={multiSelectStyles.select} tabIndex="1" onClick={onClickSelectHandler} id="port" >
-            <p>Выбрано <span>{1}</span></p>
+            <p>Выбрано <span>{selectedValues.length}</span></p>
           </div>
           {
-            isSelectActive &&
+            isSelectOpen &&
             <div className={multiSelectStyles.menu}>
-              {options.map((item, index) => <p key={index}>{item.label}</p>)}
+                {options.map((item, index) =>
+                  <p key={index} onClick={() => onSelectedItemClick(item.value)}>
+                    {isSelected(item.value)
+                      ?
+                      <IconCheckBoxYes />
+                      :
+                      <IconCheckBoxNo />
+                     }
+                    {item.label}
+                  </p>)
+                }
             </div>
           }
         </div>
