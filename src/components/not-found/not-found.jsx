baea938d152b8d68d3fd696b354a73df06af2dc3
Andrew-Sparrow <8911102@gmail.com>
2023-04-04 21:32:08 +0500

    adds styles for not-found page

--- src/components/not-found/not-found.jsx
+++ src/components/not-found/not-found.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import styles from './not-found.module.scss';
 
 function NotFound() {
   return (
-    <div style={{ margin: 'auto', width: '200px' }}>
-      <h1 style={{ textAlign: 'center' }}>404</h1>
-      <h2 className="not-found" >Page Not Found</h2>
-      <Link className="not-found__link" to={AppRoute.MAIN}>Return to main page</Link>
+    <div className={styles.app}>
+      <div className={styles.main}>
+        <h1 className={styles.title}>404</h1>
+        <h2 className={styles.paragraph}>Page Not Found</h2>
+        <Link className={styles.link} to={AppRoute.MAIN}>Return to main page</Link>
+      </div>
     </div>
   );
 }
