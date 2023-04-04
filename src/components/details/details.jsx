ced61e1ba28d3e87a2fc605ff2e91b27f9b144f6
Andrew-Sparrow <8911102@gmail.com>
2023-04-04 22:03:22 +0500

    start details page developing

--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import styles from './details.module.scss';
+
 
 const Details = (props) => {
   const { id } = useParams();
 
   return (
-    <div>Details - {id}</div>
+    <div className={styles.app}>
+      <div className={styles.main}>
+        <h2 className={styles.title}>404</h2>
+        <div>Details - {id}</div>
+      </div>
+    </div>
   )
 }
 
