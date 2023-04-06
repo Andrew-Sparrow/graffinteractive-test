4e198086a0b956b428214830fb22a19e44ca6c53
Andrew-Sparrow <8911102@gmail.com>
2023-04-06 23:23:21 +0500

    adds color bg for hover menu-items in filter component

--- src/components/button-filter-container/button-filter-container.jsx
+++ src/components/button-filter-container/button-filter-container.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { HandySvg } from 'handy-svg';
 import iconFilterSVG from '../../img/icons/Filters.svg';
+import { useMediaQuery } from 'react-responsive';
 
 import styles from './button-filter-container.module.scss';
+
 
 const IconFilter = () => (
   <HandySvg
@@ -13,11 +15,17 @@ const IconFilter = () => (
 );
 
 const ButtonFilterContainer = (props) => {
-  const { isFilterOpen, onClick } = props;
+  const { onClick } = props;
+
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1920px)'
+  })
 
   return (
     <div>
-      <button className={styles.button} type="button" onClick={onClick}><IconFilter /><span className={styles.text}></span>Фильтры</button>
+      <button className={styles.button} type="button" onClick={onClick}>
+        <IconFilter /><span className={styles.text}></span>Фильтры
+      </button>
     </div>
   )
 };
